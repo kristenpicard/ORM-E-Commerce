efcb108643b3049f04d54dfc701ff090778782e0
Kristen Picard <kristenpicard93@gmail.com>
2021-04-18 15:41:33 -0400

    Working on index.js in models and server.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,38 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // K10: Unique?
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  // K10: Unique?
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // Alias for when data is retrieved
+  as: "?1",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // Alias for when data is retrieved
+  as: "?2",
+});
 
 module.exports = {
   Product,
