40b671eb58690c2d3e359d0aa7d357ad545c8e28
Kristen Picard <kristenpicard93@gmail.com>
2021-04-18 16:39:21 -0400

    Started on get all routes but cant connect to server to test

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,12 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll();
+    res.status(200).json(poductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
