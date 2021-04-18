a8358689d875c2bd76da839bf3513b0738ac1bf6
Kristen Picard <kristenpicard93@gmail.com>
2021-04-18 16:09:44 -0400

    Added foreign keys to index.js model

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,33 +5,31 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+// CATEGORY CAN HAVE MULTIPLE PRODUCTS
+// BUT A PRODUCT CAN ONLY BELONG TO ONE CATEGORY
 Product.belongsTo(Category, {
-  // K10: Unique?
+  foreignKey: "category_id",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  // K10: Unique?
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-    unique: false,
   },
-  // Alias for when data is retrieved
-  as: "?1",
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-    unique: false,
   },
-  // Alias for when data is retrieved
-  as: "?2",
+  foreignKey: "tag_id",
 });
 
 module.exports = {
