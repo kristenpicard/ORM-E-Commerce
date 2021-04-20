4f361d33b5e278a1e202979f51ef3a73df7cb642
Kristen Picard <kristenpicard93@gmail.com>
2021-04-20 11:11:06 -0400

    Got categories by id get working

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
-// -------------------------------K10 not ready
 router.get("/", async (req, res) => {
   // find all tags
   try {
